7e4896e3f8cd12c23efb5713dca55e033347c1e2
DevStar94 <kardzavaryan@gmail.com>
2020-12-14 02:38:58 -0800

    delete

--- hooks/pre_gen_project.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-using_docker = '{{ cookiecutter.use_docker }}' == 'y'
-using_postgres = '{{ cookiecutter.database }}' == 'postgres'
-
-if using_docker and not using_postgres:
-    print("ERROR: Docker set up requires using postgres! It will run in a docker container.")
-    sys.exit(1)
--- {{cookiecutter.project_slug}}/pegasus/apps/examples/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PegasusExamplesConfig(AppConfig):
-    name = 'pegasus.apps.examples'
-    label = 'pegasus_examples'

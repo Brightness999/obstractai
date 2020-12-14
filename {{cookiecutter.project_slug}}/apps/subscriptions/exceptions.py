b7f002c8a9be2800ad69c544550759ff992476d2
Brightness999 <brightness0804@gmail.com>
2020-12-14 02:38:58 -0800

    delete

--- {{cookiecutter.project_slug}}/apps/subscriptions/exceptions.py
+++
@@ -1,4 +0,0 @@
-
-
-class SubscriptionConfigError(Exception):
-    pass
--- {{cookiecutter.project_slug}}/apps/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-app_name = 'users'
-urlpatterns = [
-    path(r'profile/', views.profile, name='user_profile'),
-    path(r'profile/upload-image/', views.upload_profile_image, name='upload_profile_image'),
-]

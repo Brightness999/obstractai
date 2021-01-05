b9a0c1acf2ded0098d63dadc038e2bd84d2979ec
Brightness999 <brightness0804@gmail.com>
2021-01-05 09:42:43 -0800

    change

--- pega/settings.py
+++ pega/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'mTMhZsVDjurGMIIswFuMKjxiBjomVaFEddqLmpCk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '178.62.35.174', 'localhost']
+ALLOWED_HOSTS = ['178.62.35.174', '0.0.0.0', 'localhost']
 
 
 # Application definition

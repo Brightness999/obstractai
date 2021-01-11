0a1984488458b28a7d65f74ea615b9bb213998e0
Brightness999 <brightness0804@gmail.com>
2021-01-11 17:05:59 -0300

    chore:chaged the domain name setting

--- pega/settings.py
+++ pega/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'mTMhZsVDjurGMIIswFuMKjxiBjomVaFEddqLmpCk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['obstractai.com', '178.62.35.174', '0.0.0.0', 'localhost']
+ALLOWED_HOSTS = [ '178.62.35.174', '.obstractai.com']
 
 
 # Application definition

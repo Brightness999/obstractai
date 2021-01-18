bc71802682a94bc56f24aa16fe07abf25097021c
good-lancer <developer613@sourceplay.com>
2021-01-18 21:46:39 +0000

    show tables in admin panel

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+@admin.register(Feeds)
+class FeedAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'tags', 'url', 'description', 'intelgroup')
+
+
+@admin.register(IntelGroups)
+class IntelGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'plan')
+
+
+@admin.register(Attributes)
+class AttributeAdmin(admin.ModelAdmin):
+    list_display = ('attribute', 'value', 'api_attribute', 'api_value', 'words_matched', 'user', 'intelgroup', 'enabled')
+
+
--- project/models/intelgroups.py
+++ project/models/intelgroups.py
@@ -1,4 +1,6 @@
 import uuid
+import random
+import string
 
 from django.conf import settings
 from django.db import models
@@ -11,8 +13,20 @@ from .plans import Plans
 
 
 class IntelGroups(BaseModel):
-    uniqueid = models.UUIDField(default=uuid.uuid4)
+    uniqueid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=100, default='')
     description = models.CharField(max_length=100, default='')
     plan = models.ForeignKey(Subscription, on_delete=models.CASCADE, null=True)
-   
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if self.name is None or self.name == '':
+            letters = string.digits
+            self.name = 'Intel Group ' + ''. join(random.choice(letters) for i in range(10))
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = "IntelGroups"
+

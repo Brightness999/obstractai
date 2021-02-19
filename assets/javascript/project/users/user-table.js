38a172763ead54ff539a3dd72837e6b5c85f63ed
Brightness999 <brightness0804@gmail.com>
2021-02-19 08:11:32 -0800

    feat:updated the manage users

--- assets/javascript/project/users/user-table.js
+++ assets/javascript/project/users/user-table.js
@@ -4,9 +4,10 @@ import { Tr, Td } from 'react-super-responsive-table';
 const UserTable = function (props) {
 	let role = ''; let admin = ''; let group = ''; let you='';
 	switch(props.role){
-		case 0: role = 'Invite Pending'; admin = 'Revoke invite'; break;
+		case 0: role = 'Invite Pending'; group = 'Revoke invite'; break;
 		case 1: role = 'Member'; admin = 'Make admin'; group = 'Delete from group'; break; 
 		case 2: role = 'Admin'; admin ='Remove admin'; group = 'Delete from group'; break;
+		case 4: role = 'Request Invite'; group ='Revoke request'; break;
 	}
 	if(props.myId == props.user_id){
 		you = '(you)';
@@ -23,15 +24,13 @@ const UserTable = function (props) {
 						<div className="button is-text">
 						<span>
 							<a onClick={() => {
-								if(props.role == 0)
-									props.delete(props.index);
-								else props.adminUser(props.role, props.id,);
-								}}>
+								props.adminUser(props.role, props.id,);
+							}}>
 								<span>{admin}</span>
-							</a>{ props.role == 0 || props.user_id == props.myId ? '': ' / ' }
+							</a>{ props.role == 0 || props.role == 4 || props.user_id == props.myId ? '': ' / ' }
 							<a onClick={() => props.delete(props.index)}>
 								<span>{group}</span>
-						</a>
+							</a>
 						</span>
 					</div>
 						

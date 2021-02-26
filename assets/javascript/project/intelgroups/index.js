0465b3374d5743bbbdcfc08ebeeaf1bc76db52ff
Brightness999 <brightness0804@gmail.com>
2021-02-25 17:35:40 -0800

    chore:fix issue 57,59,89

--- assets/javascript/project/intelgroups/index.js
+++ assets/javascript/project/intelgroups/index.js
@@ -6,28 +6,10 @@ import HelpIcon from '@material-ui/icons/Help';
 import { yellow } from '@material-ui/core/colors';
 
 const IntelGroups = function(props) {
-	const [users, setUsers] = useState([]);
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [tags, setTags] = useState([]);
   const history = useHistory();  
-  const userOptions = users.map((user)=>({
-    id: user.id,
-    name: user.email
-  }));
-
-  useEffect(() => {
-		fetch('/api/users',{
-			method: 'get',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			credentials: 'same-origin',
-		}).then(res=>{return res.json()})
-		.then(res=>{
-			setUsers(res);
-		})
-  }, []);
   
   const reacttag= React.createRef();
   const onDelete= (i)=> {
@@ -37,7 +19,7 @@ const IntelGroups = function(props) {
     }
   
   const onAddition = (tag)=> {
-    let mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    let mailformat = /^([A-Za-z0-9_\-\.\+])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
     if(tag.name.match(mailformat)){
       var temp = [].concat(tags, tag)
       setTags(temp)
@@ -45,23 +27,16 @@ const IntelGroups = function(props) {
   }
 
   const saveIntelgroup = function() {
-    const userids = [];
     const emails = [];
     tags.forEach(tag => {
-      if(Boolean(tag.id)) {
-        userids.push(tag.id)
-      }
-      else {
-        emails.push(tag.name)
-      }
+      emails.push(tag.name);
     });
     let params = {
       name: name,
       description: description,
-      userids: userids,
       emails: emails
     };
-    if(name != '' && description != '' && (userids != []) || emails != []){
+    if(name != '' && description != '' && emails.length > 0){
       fetch('/api/intelgroups', {
         method: 'post',
         headers: {
@@ -78,72 +53,88 @@ const IntelGroups = function(props) {
     }
   };
 
-  return (
-    <Container>
-    <section className="section app-card">
-      <h2 className="subtitle">Create New Intel Group</h2>
-      <div className="field column is-two-thirds">
-        <label className="label">Name</label>
-        <TextField
-          placeholder="Name of intel group"
-          className="column is-three-quarters"
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        /><Tooltip title="Name to be displayed in UI" arrow><HelpIcon className="mt-5" style={{color:yellow[900]}} fontSize="large"/></Tooltip>
-      
-        <label className="label">Description</label>
-        <TextField
-          placeholder="Description of intel group"
-          className="column is-three-quarters"
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-          value={description}
-          onChange={(event) => setDescription(event.target.value)}
-        /><Tooltip title="Description to be displayed in UI" arrow><HelpIcon className="mt-5" style={{color:yellow[900]}} fontSize="large"/></Tooltip>
-        <div style={{zIndex:1}}>
-          <label className="label mb-5">Invite Users</label>
-          <Grid container>
-            <Grid item xs={9}>
-              <ReactTags
-                ref={reacttag}
-                tags={tags}
-                suggestions={userOptions}
-                onDelete={onDelete}
-                placeholderText="Comma separated list of emails for users to invite"
-                onAddition={onAddition}
-                allowNew={true}
-                delimiters={['Enter', 'Tab', ',', ' ']}
-              />
+  if(props.currentrole.role ==0){
+      return (
+          <div className='app-card has-text-centered'>
+              <div className="lds-ripple"><div></div><div></div></div>
+              <p className="subtitle is-3">! You have an invitation to <span className="title is-3 has-text-primary">{props.currentrole.intelgroup.name}</span> pending. <Link className="muted-link subtitle is-3" to="/account" >Click here to accept.</Link></p>
+          </div>
+      )
+  }
+  if(props.currentrole.role == 4){
+      return(
+          <div className='section has-text-centered'>
+              <p className="subtitle is-3">Your request for <span className="title is-3 has-text-primary">{props.currentrole.intelgroup.name}</span> Intel Group has not been accepted yet.</p>
+          </div>
+      )
+  }
+  else {
+    return (
+      <Container>
+      <section className="section app-card">
+        <h2 className="subtitle">Create New Intel Group</h2>
+        <div className="field column is-two-thirds">
+          <label className="label">Name</label>
+          <TextField
+            placeholder="Name of intel group"
+            className="column is-three-quarters"
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="outlined"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          /><Tooltip title="Name to be displayed in UI" arrow><HelpIcon className="mt-5" style={{color:yellow[900]}} fontSize="large"/></Tooltip>
+        
+          <label className="label">Description</label>
+          <TextField
+            placeholder="Description of intel group"
+            className="column is-three-quarters"
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="outlined"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          /><Tooltip title="Description to be displayed in UI" arrow><HelpIcon className="mt-5" style={{color:yellow[900]}} fontSize="large"/></Tooltip>
+          <div style={{zIndex:1}}>
+            <label className="label mb-5">Invite Users</label>
+            <Grid container>
+              <Grid item xs={9}>
+                <ReactTags
+                  ref={reacttag}
+                  tags={tags}
+                  onDelete={onDelete}
+                  placeholderText="Comma separated list of emails for users to invite"
+                  onAddition={onAddition}
+                  allowNew={true}
+                  delimiters={['Enter', 'Tab', ',', ' ']}
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <Tooltip title="Users you want to invite" arrow><HelpIcon className="mt-2" style={{color:yellow[900]}} fontSize="large"/></Tooltip>
+              </Grid>
             </Grid>
-            <Grid item xs={3}>
-              <Tooltip title="Users you want to invite" arrow><HelpIcon className="mt-2" style={{color:yellow[900]}} fontSize="large"/></Tooltip>
-            </Grid>
-          </Grid>
+          </div>
         </div>
-      </div>
-      <div className="field is-grouped">
-        <div className="control">
-          <button type='button' className="button is-primary" onClick={() => saveIntelgroup()} >
-            <span>Create intel group</span>
-          </button>
-        </div>
-        <div className="control">
-            <button className="button is-text" onClick={()=>{history.goBack()}}>
-              <span>Cancel</span>
+        <div className="field is-grouped">
+          <div className="control">
+            <button type='button' className="button is-primary" onClick={() => saveIntelgroup()} >
+              <span>Create intel group</span>
             </button>
+          </div>
+          <div className="control">
+              <button className="button is-text" onClick={()=>{history.goBack()}}>
+                <span>Cancel</span>
+              </button>
+          </div>
         </div>
-      </div>
-    </section>
-    </Container>
-  );
+      </section>
+      </Container>
+    );
+  }
 };
 
 export default IntelGroups;

8ab4e31554836a16e4b95e2ffa59290339dbc028
Brightness999 <brightness0804@gmail.com>
2021-03-01 12:45:45 -0800

    chore:fix the issue 63

--- project/urls.py
+++ project/urls.py
@@ -12,9 +12,3 @@ urlpatterns = [
     path(r'', views.Home.as_view(), name='home'),
     path(r'<path:path>', views.Home.as_view(), name='home'),
 ]
-
-# router = routers.DefaultRouter()
-# router.register(r'api/intelgroups', views.IntelGroupViewSet)
-# router.register(r'api/intelgrouprole', views.IntelGroupRoleViewSet)
-# router.register(r'api/users', views.UserViewSet)
-# urlpatterns += router.urls
